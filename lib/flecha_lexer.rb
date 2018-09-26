9bf28028830b2e652b222b45ec7e8635db675d96
David Correa <david.correa@10pines.com>
2018-09-26 01:30:13 -0300

    Se agrega reconocimiento de identificadores en minuscula y mayuscula

--- lib/flecha_lexer.rb
+++ lib/flecha_lexer.rb
@@ -4,7 +4,12 @@ class FlechaLexer < RLTK::Lexer
   rule(/\s/)     # Espacios.
   rule(/--.*\n/) # Comentarios
 
+  rule(/[a-z][_a-zA-Z0-9]*/) { :LOWERID } # Variables, constantes y funciones.
+  rule(/[A-Z][_a-zA-Z0-9]*/) { :UPPERID } # Constructores.
+
   def tokenize(string)
-    self.lex(string)
+    tokens = self.lex(string)
+    tokens.delete_if { |token| token.type.eql? :EOS }
+    tokens
   end
 end
--- spec/flecha_parser_spec.rb
+++ spec/flecha_parser_spec.rb
@@ -2,42 +2,62 @@ require 'spec_helper'
 require_relative '../lib/flecha_lexer'
 
 describe 'Flecha Reader' do
-  shared_examples 'solo devuelve el token End of String (EOS)' do
-    it 'solo devuelve el token de End of String (EOS)' do
+  shared_examples 'no se genera ningún token' do
+    it 'no devuelve ningún token' do
+      tokens = FlechaLexer.new.tokenize(string)
+
+      expect(tokens.size).to eq(0)
+    end
+  end
+
+  shared_examples 'se genera un token' do | token |
+    it 'no devuelve ningún token' do
       tokens = FlechaLexer.new.tokenize(string)
 
       expect(tokens.size).to eq(1)
-      expect(tokens.first.type).to be(:EOS)
+      expect(tokens.first.type).to eq(token)
     end
   end
 
   context 'cuando hay espacios vacios' do
     let(:string) { ' ' }
 
-    include_examples 'solo devuelve el token End of String (EOS)'
+    include_examples 'no se genera ningún token'
   end
 
   context 'cuando hay tabs' do
     let(:string) { "\t" }
 
-    include_examples 'solo devuelve el token End of String (EOS)'
+    include_examples 'no se genera ningún token'
   end
 
   context 'cuando hay saltos de linea' do
     let(:string) { "\n" }
 
-    include_examples 'solo devuelve el token End of String (EOS)'
+    include_examples 'no se genera ningún token'
   end
 
   context 'cuando hay saltos de linea' do
     let(:string) { "\r" }
 
-    include_examples 'solo devuelve el token End of String (EOS)'
+    include_examples 'no se genera ningún token'
   end
 
   context 'cuando hay comentarios' do
     let(:string) { "-- Esto es un comentario \n" }
 
-    include_examples 'solo devuelve el token End of String (EOS)'
+    include_examples 'no se genera ningún token'
+  end
+
+  context 'cuando hay un identificador que comienza en minuscula' do
+    let(:string) { 'identificador' }
+
+    include_examples 'se genera un token', :LOWERID
+  end
+
+  context 'cuando hay un identificador que comienza en mayuscula' do
+    let(:string) { 'Identificador' }
+
+    include_examples 'se genera un token', :UPPERID
   end
 end
