ac9bd5f8d610ec344a61e65e32b07e7beb7eac68
Julian Calvento <juliancalvento@gmail.com>
2018-10-18 01:41:59 -0300

    Arreglo cosas

--- lib/flecha_parser.rb
+++ lib/flecha_parser.rb
@@ -1,36 +1,93 @@
 require 'rltk/parser'
 
 class FlechaParser < RLTK::Parser
-  left :AND, :OR, :NE, :EQ, :GE, :LE, :GT, :LT, :PLUS, :MINUS,:TIMES, :DIV, :MOD
+  left :AND, :OR, :NE, :EQ, :GE, :LE, :GT, :LT, :PLUS, :MINUS, :TIMES, :DIV, :MOD
 
-  # production(:expresion_case) do
-  #   clause('CASE expresion_interna ramas_case') do |_, expresion_interna, ramas_case|
-  #     ['ExprCase', expresion_interna, ramas_case]
-  #   end
-  # end
-  #
-  # production(:ramas_case) do
-  #   clause('') { [] }
-  #   clause('ramas_case rama_case') { |ramas, rama| [rama] + ramas }
-  # end
-  #
-  # production(:rama_case) do
-  #   clause('PIPE UPPERID ARROW expresion_interna') do |_, constructor, _, expresion_interna|
-  #     ['CaseBranch', constructor, [], expresion_interna]
-  #   end
-  # end
+  class Environment < Environment
+    def generar_lambda(expresion, parametros)
+      parametros.reverse.inject(expresion) do |result, parametro|
+        ["ExprLambda", parametro, result]
+      end
+    end
+  end
 
-  # production(:expresion_lambda) do
-  #   clause('LAMBDA parametros ARROW expresion_interna') { |_lambda, parametros, _arrow, expresion| ["ExprLambda", parametros, expresion] }
-  # end
-  #
-  # production(:parametros) do
-  #   clause('') { [] }
-  #   clause('expresion_atomica parametros') { |expr, params_list| expr + params_list }
-  # end
+  production(:programa) do
+    clause('') { [] }
+    clause('programa definicion') { |programa, definicion| programa + [definicion] }
+  end
+
+  production(:definicion) do
+    clause('DEF LOWERID DEFEQ expresion') do |_d, lower_id, _df, expr|
+      ['Def', lower_id, expr]
+    end
+
+    clause('DEF LOWERID parametros DEFEQ expresion') do |_d, lower_id, params, _df, expr|
+      ['Def', lower_id, generar_lambda(expr, params)]
+    end
+  end
+
+  production(:expresion) do
+    clause('expresion_externa') { |expr| expr }
+    clause('expresion_externa SEMICOLON expresion') do |expr_externa, _semi, expresion|
+      ["ExprLet", "_", expr_externa, expresion]
+    end
+  end
+
+  production(:expresion_externa) do
+    clause('expresion_if') { |expr| expr }
+    clause('expresion_case') { |expr| expr }
+    clause('expresion_let') { |expr| expr }
+    clause('expresion_lambda') { |expr| expr }
+    clause('expresion_interna') { |expr| expr }
+  end
+
+  production(:expresion_if) do
+    clause('IF expresion_interna THEN expresion_interna ramas_else') do |_, expr_1, _, expr_2, ramas_else|
+      ['ExprCase', expr_1, [['CaseBranch', 'True', [], expr_2], ramas_else]]
+    end
+  end
+
+  production(:ramas_else) do
+    clause('ELSE expresion_interna') do |_else, expr|
+      ['CaseBranch', 'False', [], expr]
+    end
+    clause('ELIF expresion_interna THEN expresion_interna ramas_else') do |_, expr_1, _, expr_2, ramas_else|
+      ['CaseBranch', 'False', [], ['ExprCase', expr_1, [['CaseBranch', 'True', [], expr_2], ramas_else]]]
+    end
+  end
+
+  production(:expresion_lambda) do
+    clause('LAMBDA parametros ARROW expresion_externa') do |_, parametros, _, expresion|
+      generar_lambda(expresion, parametros)
+    end
+  end
+
+  production(:parametros) do
+    clause('') { [] }
+    clause('LOWERID parametros') { |expr, params_list| [expr] + params_list }
+  end
+
+  production(:expresion_case) do
+    clause('CASE expresion_interna ramas_case') do |_, expresion_interna, ramas_case|
+      ['ExprCase', expresion_interna, ramas_case]
+    end
+  end
+
+  production(:ramas_case) do
+    clause('') { [] }
+    clause('ramas_case rama_case') { |ramas, rama| ramas + [rama] }
+  end
+
+  production(:rama_case) do
+    clause('PIPE UPPERID ARROW expresion_interna') do |_, constructor, _, expresion_interna|
+      ['CaseBranch', constructor, [], expresion_interna]
+    end
+  end
 
   production(:expresion_interna) do
-    clause('expresion_aplicacion') { |expresion_aplicacion| expresion_aplicacion }
+    clause('expresion_aplicacion') do |expresion_aplicacion|
+      expresion_aplicacion
+    end
 
     clause('expresion_interna operador_binario expresion_interna') do | expresion_interna_l, operador_binario, expresion_interna_r|
       ['ExprApply', ['ExprApply', operador_binario, expresion_interna_l], expresion_interna_r]
@@ -38,6 +95,12 @@ class FlechaParser < RLTK::Parser
 
     clause('operador_unario expresion_interna') do |operador_unario, expresion_interna|
       ['ExprApply', operador_unario, expresion_interna]
+    end
+  end
+
+  production(:expresion_let) do
+    clause('LET LOWERID DEFEQ expresion_interna IN expresion_externa') do |_, lower_id, _, expr_int, _, expr_ext|
+      ["ExprLet", lower_id, expr_int, expr_ext]
     end
   end
 
@@ -67,17 +130,22 @@ class FlechaParser < RLTK::Parser
     clause('UPPERID') { |upper_id|  ['ExprConstructor', upper_id] }
     clause('NUMBER')  { |number|    ['ExprNumber', number] }
     clause('CHAR')    { |character| ['ExprChar', character] }
-    # clause('LPAREN expresion RPAREN') { |_, expresion, _| expresion }
+    clause('LPAREN expresion RPAREN') { |_, expresion, _| expresion }
     clause('STRING') do |string|
       string.split('').reverse.inject(%w(ExprConstructor Nil)) do | product, character |
-        ['ExprApply', ['ExprApply', %w(ExprConstructor Cons), ['ExprChar', character]], product]
+        ['ExprApply', ['ExprApply', %w(ExprConstructor Cons), ['ExprChar', character.ord]], product]
       end
     end
   end
 
   production(:expresion_aplicacion) do
-    clause('expresion_atomica') { |expresion_atomica| expresion_atomica }
-    clause('expresion_aplicacion expresion_atomica') { |_expresion_aplicacion, expresion_atomica | ['ExprApply', expresion_atomica] }
+    clause('expresion_atomica') do |expresion_atomica|
+      expresion_atomica
+    end
+
+    clause('expresion_aplicacion expresion_atomica') do |expresion_aplicacion, expresion_atomica|
+      ['ExprApply', expresion_aplicacion, expresion_atomica]
+    end
   end
 
   finalize
--- spec/flecha_parser_spec.rb
+++ spec/flecha_parser_spec.rb
@@ -6,7 +6,7 @@ describe FlechaParser do
   let(:parser) { FlechaParser.new }
   let(:lexer) { FlechaLexer.new }
 
-  shared_examples 'parsea la expresión' do |string, a:|
+  shared_examples 'parsea' do |string, a:|
     it do
       tokens = lexer.lex(string)
 
@@ -14,29 +14,160 @@ describe FlechaParser do
     end
   end
 
-  it_behaves_like 'parsea la expresión', '1', a: ['ExprNumber', 1]
-  it_behaves_like 'parsea la expresión', "'a'", a: ['ExprChar', 'a'.ord]
-  it_behaves_like 'parsea la expresión', '"ho"', a: ["ExprApply", ["ExprApply", %w(ExprConstructor Cons), %w(ExprChar h)], ["ExprApply", ["ExprApply", %w(ExprConstructor Cons), %w(ExprChar o)], %w(ExprConstructor Nil)]]
-
-  it_behaves_like 'parsea la expresión', '2 + 1', a: ["ExprApply", ["ExprApply", %w(ExprVar ADD), ["ExprNumber", 2]], ["ExprNumber", 1]]
-  it_behaves_like 'parsea la expresión', '3 - 1', a: ["ExprApply", ["ExprApply", %w(ExprVar SUB), ["ExprNumber", 3]], ["ExprNumber", 1]]
-  it_behaves_like 'parsea la expresión', '4 * 12', a: ["ExprApply", ["ExprApply", %w(ExprVar MUL), ["ExprNumber", 4]], ["ExprNumber", 12]]
-  it_behaves_like 'parsea la expresión', '12 / 4', a: ["ExprApply", ["ExprApply", %w(ExprVar DIV), ["ExprNumber", 12]], ["ExprNumber", 4]]
-  it_behaves_like 'parsea la expresión', '20 % 5', a: ["ExprApply", ["ExprApply", %w(ExprVar MOD), ["ExprNumber", 20]], ["ExprNumber", 5]]
-  it_behaves_like 'parsea la expresión', '-5',     a: ["ExprApply", %w(ExprVar UMINUS), ["ExprNumber", 5]]
-
-  it_behaves_like 'parsea la expresión', 'True || False', a: ["ExprApply", ["ExprApply", %w(ExprVar OR), ["ExprConstructor", 'True']], ["ExprConstructor", 'False']]
-  it_behaves_like 'parsea la expresión', 'True && False', a: ["ExprApply", ["ExprApply", %w(ExprVar AND), ["ExprConstructor", 'True']], ["ExprConstructor", 'False']]
-  it_behaves_like 'parsea la expresión', '!True', a: ["ExprApply", %w(ExprVar NOT), ["ExprConstructor", 'True']]
-
-  it_behaves_like 'parsea la expresión', '1 != 2', a: ["ExprApply", ["ExprApply", %w(ExprVar NE), ["ExprNumber", 1]], ["ExprNumber", 2]]
-  it_behaves_like 'parsea la expresión', '2 == 2', a: ["ExprApply", ["ExprApply", %w(ExprVar EQ), ["ExprNumber", 2]], ["ExprNumber", 2]]
-  it_behaves_like 'parsea la expresión', '1 <= 2', a: ["ExprApply", ["ExprApply", %w(ExprVar LE), ["ExprNumber", 1]], ["ExprNumber", 2]]
-  it_behaves_like 'parsea la expresión', '1 >= 2', a: ["ExprApply", ["ExprApply", %w(ExprVar GE), ["ExprNumber", 1]], ["ExprNumber", 2]]
-  it_behaves_like 'parsea la expresión', '3 > 2', a: ["ExprApply", ["ExprApply", %w(ExprVar GT), ["ExprNumber", 3]], ["ExprNumber", 2]]
-  it_behaves_like 'parsea la expresión', '3 < 2', a: ["ExprApply", ["ExprApply", %w(ExprVar LT), ["ExprNumber", 3]], ["ExprNumber", 2]]
-
-  # it_behaves_like 'parsea la expresión', 'case x', a: ["ExprCase", ["ExprVar", "x"], []]
+  it_behaves_like 'parsea', 'def foo = 2 + 1', a: [['Def', 'foo', ["ExprApply", ["ExprApply", %w(ExprVar ADD), ["ExprNumber", 2]], ["ExprNumber", 1]]]]
+  # it_behaves_like 'parsea', '3 - 1', a: ["ExprApply", ["ExprApply", %w(ExprVar SUB), ["ExprNumber", 3]], ["ExprNumber", 1]]
+  # it_behaves_like 'parsea', '4 * 12', a: ["ExprApply", ["ExprApply", %w(ExprVar MUL), ["ExprNumber", 4]], ["ExprNumber", 12]]
+  # it_behaves_like 'parsea', '12 / 4', a: ["ExprApply", ["ExprApply", %w(ExprVar DIV), ["ExprNumber", 12]], ["ExprNumber", 4]]
+  # it_behaves_like 'parsea', '20 % 5', a: ["ExprApply", ["ExprApply", %w(ExprVar MOD), ["ExprNumber", 20]], ["ExprNumber", 5]]
+  # it_behaves_like 'parsea', '-5',     a: ["ExprApply", %w(ExprVar UMINUS), ["ExprNumber", 5]]
   #
-  # it_behaves_like 'parsea la expresión', '\ x -> x + 2', a: ["ExprLambda", %w(ExprVar x), ["ExprApply", ["ExprApply", %w(ExprVar ADD), %w(ExprVar x)], ["ExprNumber", 2]]]
+  # it_behaves_like 'parsea', 'True || False', a: ["ExprApply", ["ExprApply", %w(ExprVar OR), ["ExprConstructor", 'True']], ["ExprConstructor", 'False']]
+  # it_behaves_like 'parsea', 'True && False', a: ["ExprApply", ["ExprApply", %w(ExprVar AND), ["ExprConstructor", 'True']], ["ExprConstructor", 'False']]
+  # it_behaves_like 'parsea', '!True', a: ["ExprApply", %w(ExprVar NOT), ["ExprConstructor", 'True']]
+  #
+  # it_behaves_like 'parsea', '1 != 2', a: ["ExprApply", ["ExprApply", %w(ExprVar NE), ["ExprNumber", 1]], ["ExprNumber", 2]]
+  # it_behaves_like 'parsea', '2 == 2', a: ["ExprApply", ["ExprApply", %w(ExprVar EQ), ["ExprNumber", 2]], ["ExprNumber", 2]]
+  # it_behaves_like 'parsea', '1 <= 2', a: ["ExprApply", ["ExprApply", %w(ExprVar LE), ["ExprNumber", 1]], ["ExprNumber", 2]]
+  # it_behaves_like 'parsea', '1 >= 2', a: ["ExprApply", ["ExprApply", %w(ExprVar GE), ["ExprNumber", 1]], ["ExprNumber", 2]]
+  # it_behaves_like 'parsea', '3 > 2', a: ["ExprApply", ["ExprApply", %w(ExprVar GT), ["ExprNumber", 3]], ["ExprNumber", 2]]
+  # it_behaves_like 'parsea', '3 < 2', a: ["ExprApply", ["ExprApply", %w(ExprVar LT), ["ExprNumber", 3]], ["ExprNumber", 2]]
+  
+  it_behaves_like 'parsea', '', a: []
+
+  it_behaves_like 'parsea', 'def a=A', a: [["Def", "a",
+                                                         ["ExprConstructor", "A"]
+                                                        ]]
+
+  it_behaves_like 'parsea', "def a = 'a' def z = 'z'", a: [["Def", "a",
+                                                                         ["ExprChar", 97]
+                                                                        ],
+                                                                        ["Def", "z",
+                                                                         ["ExprChar", 122]
+                                                                        ]]
+
+  it_behaves_like 'parsea', "def lista123 = Cons 1 (Cons 2 (Cons 3 Nil))", a: [["Def", "lista123",
+                                                                                                ["ExprApply",
+                                                                                                 ["ExprApply",
+                                                                                                  ["ExprConstructor", "Cons"],
+                                                                                                  ["ExprNumber", 1]
+                                                                                                 ],
+                                                                                                 ["ExprApply",
+                                                                                                  ["ExprApply",
+                                                                                                   ["ExprConstructor", "Cons"],
+                                                                                                   ["ExprNumber", 2]
+                                                                                                  ],
+                                                                                                  ["ExprApply",
+                                                                                                   ["ExprApply",
+                                                                                                    ["ExprConstructor", "Cons"],
+                                                                                                    ["ExprNumber", 3]
+                                                                                                   ],
+                                                                                                   ["ExprConstructor", "Nil"]
+                                                                                                  ]
+                                                                                                 ]
+                                                                                                ]
+                                                                                               ]]
+
+  it_behaves_like 'parsea', 'def variable = 2', a: [["Def", "variable", ["ExprNumber", 2]]]
+
+  it_behaves_like 'parsea', 'def suma x = x + 2', a: [["Def", "suma", ["ExprLambda", 'x', ["ExprApply", ["ExprApply", %w(ExprVar ADD), ["ExprVar", 'x']], ["ExprNumber", 2]]]]]
+
+  it_behaves_like 'parsea', 'def t1 = a ; b', a: [
+                                                                ["Def", "t1",
+                                                                  ["ExprLet", "_",
+                                                                    ["ExprVar", "a"],
+                                                                    ["ExprVar", "b"]
+                                                                  ]
+                                                                ]
+                                                              ]
+
+  it_behaves_like 'parsea', 'def t5 = case x
+         | X1 -> a
+         | X2 -> b
+         | X3 -> c', a: [["Def", "t5",
+                          ["ExprCase",
+                           ["ExprVar", "x"],
+                           [
+                             ["CaseBranch", "X1", [],
+                              ["ExprVar", "a"]
+                             ],
+                             ["CaseBranch", "X2", [],
+                              ["ExprVar", "b"]
+                             ],
+                             ["CaseBranch", "X3", [],
+                              ["ExprVar", "c"]
+                             ]
+                           ]
+                          ]
+                                                     ]]
+
+  it_behaves_like 'parsea', 'def t4 = \ x y -> y', a: [["Def", "t4",
+                                                                    ["ExprLambda", "x",
+                                                                     ["ExprLambda", "y",
+                                                                      ["ExprVar", "y"]
+                                                                     ]
+                                                                    ]
+                                                                    ]]
+
+  it_behaves_like 'parsea', 'def t1 = let x = y in z', a: [["Def", "t1",
+                                                                         ["ExprLet", "x",
+                                                                          ["ExprVar", "y"],
+                                                                          ["ExprVar", "z"]
+                                                                         ]
+                                                                        ]]
+
+  it_behaves_like 'parsea', 'def t1 = if x then y else z', a: [["Def", "t1",
+                                                                             ["ExprCase",
+                                                                               ["ExprVar", "x"],
+                                                                               [
+                                                                                 ["CaseBranch", "True", [], ["ExprVar", "y"]],
+                                                                                 ["CaseBranch", "False", [], ["ExprVar", "z"]]
+                                                                               ]
+                                                                              ]
+                                                                            ]]
+
+  it_behaves_like 'parsea', 'def t2 = if x1 then y1 elif x2 then y2 else z', a: [["Def", "t2",
+                                                                                               ["ExprCase",
+                                                                                                ["ExprVar", "x1"],
+                                                                                                [
+                                                                                                  ["CaseBranch", "True", [],
+                                                                                                   ["ExprVar", "y1"]
+                                                                                                  ],
+                                                                                                  ["CaseBranch", "False", [],
+                                                                                                   ["ExprCase",
+                                                                                                    ["ExprVar", "x2"],
+                                                                                                    [
+                                                                                                      ["CaseBranch", "True", [],
+                                                                                                       ["ExprVar", "y2"]
+                                                                                                      ],
+                                                                                                      ["CaseBranch", "False", [],
+                                                                                                       ["ExprVar", "z"]
+                                                                                                      ]
+                                                                                                    ]
+                                                                                                   ]
+                                                                                                  ]
+                                                                                                ]
+                                                                                               ]
+                                                                                              ]]
+
+  it_behaves_like 'parsea', 'def abc = "abc"', a: [["Def", "abc",
+                                                                 ["ExprApply",
+                                                                  ["ExprApply",
+                                                                   ["ExprConstructor", "Cons"],
+                                                                   ["ExprChar", 97]
+                                                                  ],
+                                                                  ["ExprApply",
+                                                                   ["ExprApply",
+                                                                    ["ExprConstructor", "Cons"],
+                                                                    ["ExprChar", 98]
+                                                                   ],
+                                                                   ["ExprApply",
+                                                                    ["ExprApply",
+                                                                     ["ExprConstructor", "Cons"],
+                                                                     ["ExprChar", 99]
+                                                                    ],
+                                                                    ["ExprConstructor", "Nil"]
+                                                                   ]
+                                                                  ]
+                                                                 ]
+                                                                ]]
 end
