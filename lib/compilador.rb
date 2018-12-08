6d751944c4a8ebc733ee0f0d5f5399925b948289
David Correa <david.correa@10pines.com>
2018-12-08 00:14:14 -0300

    Lambda, Apply

--- lib/compilador.rb
+++ lib/compilador.rb
@@ -1,7 +1,9 @@
 class Compilador
   def initialize
     @r_index = 0
+    @routine_index = 0
     @env = {}
+    @fun_env = {}
   end
 
   def call(program, compiled = '')
@@ -26,17 +28,17 @@ class Compilador
         "store(#{@last_used}, 0, $t)\n"\
         "mov_int($t, #{instructions[1]})\n"\
         "store(#{@last_used}, 1, $t)"
-      elsif instructions[0] == 'ExprApply'
-        "#{call [instructions[2]], result}\n"\
-        "#{call [instructions[1]], result}"
       elsif instructions[0] == 'ExprVar'
         if instructions[1] == 'unsafePrintInt'
           loaded = fresh_register
           "load(#{loaded}, #{@last_used}, 1)\n"\
           "print(#{loaded})"
+        elsif @fun_env.has_key?(instructions[1])
+          @last_used = fresh_register
+          "load(#{@last_used}, $fun, #{@fun_env[instructions[1]]})"
         else
           @last_used = fresh_register
-          "mov_reg(#{@last_used}, #{fetch_from_context instructions[1]})"\
+          "mov_reg(#{@last_used}, #{fetch_from_context instructions[1]})"
         end
       elsif instructions[0] == 'ExprLet'
         @let = true
@@ -49,6 +51,56 @@ class Compilador
           "#{call [instructions[2]], result}\n"\
           "mov_reg(@G_#{instructions[1]}, #{@last_used})"
         )
+      elsif instructions[0] == 'ExprLambda'
+        routine_name = fresh_routine_name
+        routine_register = fresh_register
+        @fun_env[@arg] = @fun_env.size + 2 if @arg
+        @arg = instructions[1]
+
+        "alloc(#{routine_register}, 3)\n"\
+        "mov_int($t, 3)\n"\
+        "store(#{routine_register}, 0, $t)\n"\
+        "#{routine_name}:\n"\
+        "mov_reg($fun, @fun)\n"\
+        "mov_reg($arg, @arg)\n"\
+        "#{call [instructions[2]], result}\n"\
+        "mov_reg($res, #{@last_used})\n"\
+        "mov_reg(@res, $res)\n"\
+        "return()\n"\
+        "mov_label($t, #{routine_name})\n"\
+        "store(#{routine_register}, 1, $t)\n"\
+        "store(#{routine_register}, 2, $arg)"
+      elsif instructions[0] == 'ExprApply'
+        if instructions[1][1] == 'unsafePrintInt'
+          "#{call [instructions[2]], result}\n"\
+          "#{call [instructions[1]], result}"
+        else
+          clausura = instructions[1]
+          parametro = instructions[2]
+
+          a = call([clausura], result)
+          registro_clausura = @last_used
+          b = call([parametro], a)
+          registro_parametro = @last_used
+
+          if true
+            registro_rutina = fresh_register
+
+            "#{a}\n"\
+            "#{b}\n"\
+            "load(#{registro_rutina}, #{registro_clausura}, 1)\n"\
+            "mov_reg(@fun, #{registro_clausura})\n"\
+            "mov_reg(@arg, #{registro_parametro})\n"\
+            "icall(#{registro_rutina})\n"\
+            "mov_reg(#{fresh_register}, @res)"
+          else
+            alloc($r, 3)
+            mov_int($t, 7)
+            store($r, 0, $t)
+            store($r, 1, $r1)
+            store($r, 2, $r2)
+          end
+        end
       end
     end
   end
@@ -61,7 +113,11 @@ class Compilador
   end
 
   def fetch_from_context(variable)
-    @env[variable] || "@G_#{variable}"
+    if @arg == variable
+      '$arg'
+    else
+      @env[variable] || "@G_#{variable}"
+    end
   end
 
   def fresh_register
@@ -73,5 +129,11 @@ class Compilador
 
       "$r#{fresh}"
     end
+  end
+
+  def fresh_routine_name
+    @routine_index += 1
+
+    "rtn_#{@routine_index}"
   end
 end
--- spec/compilador_spec.rb
+++ spec/compilador_spec.rb
@@ -80,11 +80,17 @@ describe Compilador do
     end
 
     it 'returns the compiled code for variable' do
-      assert_expression_is_compiled_to([["Def", "main", ["ExprApply", %w(ExprVar unsafePrintInt), %w(ExprVar foo)]]],
-        "mov_reg($r0, @G_foo)\n"\
-        "load($r1, $r0, 1)\n"\
-        "print($r1)\n"\
-        "mov_reg(@G_main, $r0)"
+      assert_expression_is_compiled_to([['Def', 'foo', ['ExprNumber', 42]], ["Def", "main", ["ExprApply", %w(ExprVar unsafePrintInt), %w(ExprVar foo)]]],
+        "alloc($r0, 2)\n"\
+        "mov_int($t, 1)\n"\
+        "store($r0, 0, $t)\n"\
+        "mov_int($t, 42)\n"\
+        "store($r0, 1, $t)\n"\
+        "mov_reg(@G_foo, $r0)"\
+        "mov_reg($r1, @G_foo)\n"\
+        "load($r2, $r1, 1)\n"\
+        "print($r2)\n"\
+        "mov_reg(@G_main, $r1)"
       )
     end
   end
@@ -101,6 +107,128 @@ describe Compilador do
         "mov_reg(@G_t, $r0)"
       )
     end
+  end
+
+  context 'Lambda' do
+    it 'returns the compiled code' do
+      assert_expression_is_compiled_to([["ExprLambda", "y", ["ExprVar", "y"]]],
+        "rtn_1:\n"\
+        "mov_reg($fun, @fun)\n"\
+        "mov_reg($arg, @arg)\n"\
+        "mov_reg($r0, $arg)\n"\
+        "mov_reg($res, $r0)\n"\
+        "mov_reg(@res, $res)\n"\
+        "return()\n"
+      )
+    end
+
+    it 'returns the compiled code' do
+      assert_expression_is_compiled_to([["ExprLambda", "x", ["ExprLambda", "y", ["ExprVar", "x"]]]],
+        "alloc($r0, 3)\n"\
+        "mov_int($t, 3)\n"\
+        "store($r0, 0, $t)\n"\
+        ""\
+        "rtn_1:\n"\
+        "mov_reg($fun, @fun)\n"\
+        "mov_reg($arg, @arg)\n"\
+        "alloc($r1, 3)\n"\
+        "mov_int($t, 3)\n"\
+        "store($r1, 0, $t)\n"\
+        ""\
+        "rtn_2:\n"\
+        "mov_reg($fun, @fun)\n"\
+        "mov_reg($arg, @arg)\n"\
+        "load($r2, $fun, 2)\n"\
+        "mov_reg($res, $r2)\n"\
+        "mov_reg(@res, $res)\n"\
+        "return()\n"\
+        ""\
+        "mov_label($t, rtn_2)\n"\
+        "store($r1, 1, $t)\n"\
+        "store($r1, 2, $arg)\n"\
+        "mov_reg($res, $r2)\n"\
+        "mov_reg(@res, $res)\n"\
+        "return()\n"\
+        ""\
+        "mov_label($t, rtn_1)\n"\
+        "store($r0, 1, $t)\n"\
+        "store($r0, 2, $arg)\n"
+      )
+    end
+  end
+
+  context 'Application - Common case' do
+    it 'returns the compiled code' do
+      assert_expression_is_compiled_to([["ExprApply", ["ExprVar", "y"], ["ExprNumber", 42]]],
+        "mov_reg($r0, @G_y)\n"\
+        "alloc($r1, 2)\n"\
+        "mov_int($t, 1)\n"\
+        "store($r1, 0, $t)\n"\
+        "mov_int($t, 42)\n"\
+        "store($r1, 1, $t)\n"\
+        "load($r2, $r0, 1)\n"\
+        "mov_reg(@fun, $r0)\n"\
+        "mov_reg(@arg, $r1)\n"\
+        "icall($r2)\n"\
+        "mov_reg($r3, @res)\n"
+      )
+    end
+  end
+
+  context 'Application - Constructor' do
+    it 'sarasa' do
+      assert_expression_is_compiled_to([[%w'ExprConstructor Cons']],
+        "rtn_1:\n"\
+        "mov_reg($fun, @fun)\n"\
+        "mov_reg($arg, @arg)\n"\
+        "mov_reg($r0, $arg)\n"\
+        "mov_reg($res, $r0)\n"\
+        "mov_reg(@res, $res)\n"\
+        "return()\n"\
+        "alloc($r0, 3)\n"\
+        "mov_int($t, 7)\n"\
+        "store($r0, 0, $t)"\
+      )
+    end
+
+    # it 'returns the compiled code' do
+    #   assert_expression_is_compiled_to([['ExprApply', %w'ExprConstructor Cons', ['ExprNumber', 42]]],
+    #
+    #   # Cons (solo)
+    #   "alloc($r0, 3)\n"\
+    #   "mov_int($t, 7)\n"\
+    #   "store($r0, 0, $t)"\
+    #
+    #   # 42
+    #   "alloc($r1, 2)\n"\
+    #     "mov_int($t, 1)\n"\
+    #     "store($r1, 0, $t)\n"\
+    #     "mov_int($t, 42)\n"\
+    #     "store($r1, 1, $t)\n"\
+    #
+    #   # \x -> Cons 42 x
+    #   "rtn_1:\n"\
+    #   "mov_reg($fun, @fun)\n"\
+    #   "mov_reg($arg, @arg)\n"\
+    #
+    #   "mov_reg($r0, $arg)\n"\
+    #
+    #   "mov_reg($res, $r0)\n"\
+    #   "mov_reg(@res, $res)\n"\
+    #   "return()\n"
+    #
+    #   # \x -> (\y -> Cons x y)
+    #   # \y -> Cons 42 y
+    #   # Cons 42 Nil
+    #
+    #
+    #     "alloc($r2, 3)\n"\
+    #     "mov_int($t, 7)\n"\
+    #     "store($r2, 0, $t)\n"\
+    #     "store($r2, 1, $r0)\n"\
+    #     "store($r2, 2, $r1)\n"
+    #   )
+    # end
   end
 
   def assert_expression_is_compiled_to(expression, expected)
